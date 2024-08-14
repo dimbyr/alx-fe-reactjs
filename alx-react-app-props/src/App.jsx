1c0d19b50a1f0f0281a4b63a696cf0a55491bd31
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-08-15 01:06:54 +0300

    Using useContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,6 +5,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
@@ -13,7 +14,9 @@ function App() {
     <>
       <Header />
       <MainContent />
-      <ProfilePage userData={userData} />
+      <UserContext.Provider  value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
       <UserProfile name ="John" age = {17} bio = "Awesome guy!"/>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
