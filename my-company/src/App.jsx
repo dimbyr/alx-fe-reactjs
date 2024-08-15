135ca3a5392fdbddbeda7dce2598ca98996f5a35
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-08-15 10:45:33 +0300

    Added navigation links

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,12 +4,14 @@ import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
      <BrowserRouter>
+     <NavBar />
        <Routes>
          <Route path='/' element= {<Home />} />
          <Route path='about'element = {<About />}/>
