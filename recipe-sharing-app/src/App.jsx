41dd26844ddc1e2634112b524100395253300798
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-08-31 11:55:01 +0300

    Added some components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/addRecipeForm';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
+        <div className="mb-6">
+          <h1 className="text-2xl font-semibold">Recipe List</h1>
+          <RecipeList />
+          <AddRecipeForm />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ recipe-sharing-app/src/components/addRecipeForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+  import useRecipeStore from '../stores/recipeStore';
+
+  const AddRecipeForm = () => {
+    const addRecipe = useRecipeStore(state => state.addRecipe);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      addRecipe({ id: Date.now(), title, description });
+      setTitle('');
+      setDescription('');
+    };
+
+    return (
+      <form className="flex justify-center items-center h-screen"
+      onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <textarea className="p-2 mr-2 w-60 border rounded-md border-gray-300"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+        />
+        <button type="submit">Add Recipe</button>
+      </form>
+    );
+  };
+
+  export default AddRecipeForm;
