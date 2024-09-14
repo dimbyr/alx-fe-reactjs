cc1f6059715a0c0871ac1d5ea00bb03afdf53fb3
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-09-14 12:42:46 +0300

    Styled the recipe cards

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,31 +1,13 @@
 import React from "react";
-import { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
-import RecipeList from "./components/RecipeList";
 
 function App() {
-  const [recipeData, setRecipeData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('data.json');
-        const data = await response.json();
-        setRecipeData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <>
       <div>
         <HomePage />
-        <RecipeList recipe={recipeData} />
       </div>
     </>
   );
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import recipeData from "../data.json";
 
 function HomePage(){
-  const [count, setCount] = useState(0)
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
+    // Since we're importing the data, no need to fetch. Set the state directly.
+    setRecipe(recipeData);
+  }, []);
+
   return(
     <>
-    <h1>Home Page</h1>
+    <h1 className="text-3xl font-bold mb-2">Home Page</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+      {recipe.map((item) => (
+        <div key={item.id} className="bg-white shadow-md rounded-lg overflow-hidden transition duration-300 transform hover:scale-105">
+          <img src={item.image} alt={item.name}             className= "w-full h-48 object-cover"
+ />
+          <h2  className="text-xl font-semibold mb-2">{item.title}</h2>
+          <p>{item.summary}</p>
+        </div>
+      ))}
+    </div>
     </>
   );
 }
