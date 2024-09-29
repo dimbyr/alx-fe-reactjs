bb6d820d64ed63b762e03c945d0685b01a5f8b2b
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-09-29 15:41:00 +0300

    Renamed registrationForm to RegistrationForm

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import RegistrationForm from './components/registrationForm'
+import RegistrationForm from './components/RegistrationForm.jsx'
 import FormikForm from './components/formikForm.jsx';
 
 function App() {
