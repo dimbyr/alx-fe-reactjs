b50bbc5a33ea0107a1bce2b70f9ac74ffaec6d51
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-09-29 16:23:19 +0300

    React query

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,18 +1,18 @@
 import { useQuery } from 'react-query';
 
 // Define a fetch function that can be used to fetch data from an API
-const fetchData = async () => {
+const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     return res.json();
 };
 
 const PostsComponent  = () => {
     // Use the useQuery hook to handle data fetching and caching
-    const { data, error, isLoading } = useQuery('fetchData', fetchData);
+    const { data, isError, isLoading } = useQuery('fetchData', fetchPosts);
     // Handle loading state
     if (isLoading) return <div>Loading...</div>;
     // Handle error state
-    if (error) return <div>Error loading data</div>;
+    if (isError) return <div>Error loading data</div>;
 
     // Render the fetched data
     return (
@@ -21,7 +21,6 @@ const PostsComponent  = () => {
                 <div key={item.id}>
                   <h1>{item.title}</h1>
                   <p>{item.body}</p>
-                
                 </div>
             ))}
         </div>
