4870eaa7921e28d71a0e69102cd21f17f51a86a1
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-10-02 09:39:22 +0300

    Added imports in Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
